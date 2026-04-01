7d431ef3da56b40d38670b2f8e4571a2ea2deb7f
Connie Michelle <cmc@conniemichelleconsulting.com>
2026-03-31 18:19:45 -0700

    Fix stale Workbox cache issues in dev by clearing old service workers

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// Clear stale service workers/caches in local dev to avoid old Workbox bundles.
+if (typeof window !== 'undefined' && window.location.hostname === 'localhost') {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then((regs) => {
+      regs.forEach((reg) => reg.unregister())
+    }).catch(() => {})
+  }
+  if ('caches' in window) {
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k)))).catch(() => {})
+  }
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
